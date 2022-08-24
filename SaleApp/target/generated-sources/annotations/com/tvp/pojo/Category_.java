84a3f4cc723192bc953b9fc89638836d011886a8
DESKTOP-U2TEBE3 <vanphuongvip29@gmail.com>
2022-08-24 09:19:22 +0700

    spring security

--- SaleApp/target/generated-sources/annotations/com/tvp/pojo/Category_.java
+++ SaleApp/target/generated-sources/annotations/com/tvp/pojo/Category_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-08-23T19:54:18")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-08-24T09:10:39")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
--- SaleApp/target/generated-sources/annotations/com/tvp/pojo/FollowRating_.java
+++ SaleApp/target/generated-sources/annotations/com/tvp/pojo/FollowRating_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-08-23T19:54:18")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-08-24T09:10:39")
 @StaticMetamodel(FollowRating.class)
 public class FollowRating_ { 
 
